892b4548471b145b0887d60c9d722f0a4f0b0f77
zkan <kan@prontomarketing.com>
2018-09-09 16:18:08 +0700

    Use Sobel X as kernel

--- convolutions/convolutions.py
+++ convolutions/convolutions.py
@@ -4,8 +4,8 @@ from skimage.exposure import rescale_intensity
 
 
 def convolve(image, K):
-    (iH, iW) = image.shape[:2]
-    (kH, kW) = k.shape[:2]
+    (iH, iW) = image.shape
+    (kH, kW) = K.shape
 
     # Allocate memory for the output image, taking care to "pad"
     # the borders of the input image so the spatial size (i.e.,
@@ -21,7 +21,7 @@ def convolve(image, K):
             output[y - pad, x - pad] = k
 
     # Rescale the output image to be in the range [0, 255]
-    output = rescale_intensity(output, in_range(0, 255))
+    output = rescale_intensity(output, in_range=(0, 255))
     output = (output * 255).astype('uint8')
 
     return output
@@ -30,6 +30,22 @@ def convolve(image, K):
 image = cv2.imread('codemania-stickers.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('Code Mania Stickers', gray)
+sobel_x = np.array(
+    (
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]
+    ),
+    dtype='int'
+)
+
+K = sobel_x
+convolve_output = convolve(gray, K)
+# When ddepth=-1, the output image will have the same depth as the source.
+opencv_output = cv2.filter2D(gray, -1, K)
+
+cv2.imshow('Original', gray)
+cv2.imshow('sobel_x - convolve', convolve_output)
+cv2.imshow('sobel_x - opencv', opencv_output)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
